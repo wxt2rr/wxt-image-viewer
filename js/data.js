55d7e47b2e10ac355a82c598d8576075cfa11211
wangxt <wangxt@ckt.cn>
2023-02-08 18:42:12 +0800

    'update'

--- js/data.js
+++ js/data.js
@@ -2,21 +2,25 @@ function submit(){
     var data = $('#input').val();
     var searchDatas = data.split(" ");
 
-    var $searchList = $("#gallery");
+    var $table = $("#table");
     var sHtml = "";
+
     for(var i in searchDatas){
         var searchData = searchDatas[i];
-w
-        var arr = searchData.split("	");
-        var url = arr[0];
-        var suggestion = arr[1];
-        var SubLabel = arr[2];
-        var Score = arr[3];
-        var type = arr[4];
 
-        sHtml += "<a href='"+ arr[0] +"' class='image'><img src='"+ arr[0] +"' alt='' /></a><h4>Suggestion:"+suggestion+"|SubLabel:"+SubLabel+"|Score:"+Score+"|原始机审类别(EvilType):"+type+"</h4>";
+        var arr = searchData.split("	");
+        sHtml += "<tr>";
+        for(var x in arr){
+            var item = arr[x];
+            if(item.indexOf("http") != -1){
+                sHtml += "<td><img width='300px' src='"+ item +"' alt='' /></td>";
+            }else{
+                sHtml += "<td>"+item+"</td>";
+            }
+        }
+        sHtml += "</tr>";
     };
 
     console.log(sHtml);
-    $searchList.html(sHtml); 
+    $table.html(sHtml); 
 }
